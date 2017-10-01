c8630c0750ffd642213949155a50f10a9c9376ba
Liam Killion <liam.killion@gmail.com>
2017-10-01 23:28:27 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,53 +18,76 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas]<<supply
+  holiday_hash[:winter][:new_years]<<supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day]<<supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  holiday_name<<supply_array
+  season<<holiday_name
+  holiday_hash<<season
+  return holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.flatten
 end
-
+# iterate through holiday_hash and print items such that your readout resembles:
+# Winter:
+#   Christmas: Lights, Wreath
+#   New Years: Party Hats
+# Summer:
+#   Fourth Of July: Fireworks, BBQ
+# etc.
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+  holiday_hash.each do |season,holidays|
+    puts "#{season.capitalize}:"
+    holidays.each do |holiday,supplies|
+      split_holiday=holiday.to_s.split
+      split_holiday.each do |word|
+        word.capitalize
+      end
+      split_holiday.join
+      puts "#{split_holiday.to_s}: #{supplies.to_s}"
+    #<IO:<STDOUT>> received :puts with unexpected arguments
+    #  expected: ("  Christmas: Lights, Wreath")
+    #       got: ("[\"christmas\"]: [\"Lights\", \"Wreath\"]")
+    end
+  end
 end
+# contacts.each do |person, data|
+#   data.each do |attribute, value|
+#     if attribute == :favorite_icecream_flavors
+#       value.delete_if do |flavor|
+#         flavor == "strawberry"
+#       end
+#     end
+#   end
+# end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
+  bbq_array = []
+  holiday_hash.each do |season,holiday_data|
+    holiday_data.each do |holiday,supplies|
+      if supplies.include?( "BBQ")
+        bbq_array<<holiday
+      end
+    end
+  end
+  return  bbq_array
 end
-
-
-
-
-
-
-
